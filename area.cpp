3217ab406bcf1e7731973a4d565feb6738e2a179
manpreet67 <34857530+manpreet67@users.noreply.github.com>
2019-10-11 18:00:49 +0530

    Create area.cpp

---
+++ area.cpp
@@ -0,0 +1,14 @@
+#include<conio.h>
+#include<iostream.h>
+int len,bre;
+float area,per;
+
+cout<<"Enter length:";
+cin>>len;
+cout<<"Enter breadth:";
+cin>>bre;
+per=2*(len+bre);
+area=len*bre;
+cout<<Perimeter and area of rectangle is:"<<per"and"<<area;
+getch();
+
